d0a24fbc58673ad56a06698b7afc3e286b7b504d
Lilianaqm <79277265+Lilianaqm@users.noreply.github.com>
2021-03-01 19:24:44 -0600

    Add files via upload

---
+++ ListaController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+use App\estudiantes; //Modelo
+
+
+class ListaController extends Controller
+{
+    public function index(Request $request)
+    {
+   $estudiante= estudiantes::all();
+    //return $estudiante;
+    return view ('VistaEstudiantes', compact('estudiante'));
+    //$texto=trim($request->get ('texto'));
+   //$estudiante=DB::table ('estudiantes')->select('Matricula','Nombre','Direccion')
+    //->where ('matricula','=',$texto)->paginate(10);
+    //return view ('VistaEstudiantes',compact('estudiante'));
+
+    //$texto=trim($request->get ('texto'));
+    //$estudiante=DB::table ('estudiantes')->select('Matricula','Nombre','Direccion')
+    //->where ('nombre','LIKE',"%$texto%")->paginate(10);
+    //return view ('VistaEstudiantes',compact('estudiante'));
+    }
+    public function destroy($id)
+        {
+    $estudiante= estudiantes::find($id);
+    //dd($estudiante);
+    $estudiante->delete();
+    return redirect()->route('Lista.index');
+    }
+}
